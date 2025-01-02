dcea9b5540c9d0b7daf39be78416ee45c31b90ab
leurjim <shaqleao@icloud.com>
2025-01-02 09:17:14 -0500

    Modificación metada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏽‍💻</text></svg>"
         />
+        <title>Blog Leao Urbina</title>
+        <meta name="description" content="Blog creado en Nextjs, Tailwind" />
       </head>
       <body className="h-screen w-screen">
         <Providers>
@@ -32,7 +34,6 @@ export default function RootLayout({
             {children}
           </main>
         </Providers>
-        ∫
       </body>
     </html>
   );
